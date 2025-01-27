e94675b8b0e70fdd2fbf6726e6479686834386fa
byron dray <byrondray8@gmail.com>
2025-01-27 11:45:19 -0800

    fix: database connection in prod

--- src/db/schema/schema.ts
+++ src/db/schema/schema.ts
@@ -14,7 +14,13 @@ import { config } from 'dotenv';
 config({ path: '.env' });
 
 const connectionString = process.env.POSTGRES_DB_URL!;
-const pool = postgres(connectionString, { max: 1 });
+const pool = postgres(connectionString, {
+  max: 1,
+  ssl: 'require',
+  connect_timeout: 10,
+  idle_timeout: 20,
+  prepare: false,
+});
 
 export const db = drizzle(pool);
 
